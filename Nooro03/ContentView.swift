02e85f4440fef062a0cea8d7b73c3caef646a609
Jon Harlan <jjharlan06@gmail.com>
2024-12-12 22:22:35 -0500

    Initial Commit

---
+++ Nooro03/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Nooro03
+//
+//  Created by MAC PAN on 12/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nooro03/Nooro03App.swift
@@ -0,0 +1,17 @@
+//
+//  Nooro03App.swift
+//  Nooro03
+//
+//  Created by MAC PAN on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Nooro03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
